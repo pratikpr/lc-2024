71dbeee49f3c6180e2da01b21a56ce44143cade8
pratikpr <pratikpr95@gmail.com>
2024-02-28 12:50:49 -0800

    [m] 122. Best Time to Buy and Sell Stock II

---
+++ top-interview-150/122_buy_sell_stock_2.py
@@ -0,0 +1,21 @@
+# You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+# On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+
+# Find and return the maximum profit you can achieve.
+
+def maxProfit(prices) -> int:
+    total_profit = 0
+
+    for i in range(1, len(prices)):
+        if prices[i] > prices[i-1]:
+            total_profit += prices[i] - prices[i-1]
+
+    return total_profit
+
+
+prices = [7,1,5,3,6,4]
+print(maxProfit(prices))
+
+prices = [1,2,3,4,5]
+print(maxProfit(prices))
